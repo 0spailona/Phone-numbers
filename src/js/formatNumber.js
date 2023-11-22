ec20ad1cf49fa04baca250c783e804dbc9cac047
Daria <n-d-p@mail.ru>
2023-11-22 20:39:50 +0300

    It works

--- src/js/formatNumber.js
+++ src/js/formatNumber.js
@@ -1,4 +1,4 @@
 export default function formatNumber(str) {
     const firstSymbol = str[0] === '+' ? '+' : '+7';
-    return `${firstSymbol}${str.slice(1).replace(/\D|/g, '')}`;
+    return `${firstSymbol}${str.slice(1).replace(/\D/g, '')}`;
 }
